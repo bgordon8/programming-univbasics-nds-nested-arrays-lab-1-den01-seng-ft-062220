50ffacff3f930f3aec5af2f28420956bc6ebaed8
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-08 04:16:54 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,7 +44,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-    CONVENTIONAL_PRODUCE[0][2] = new_value
+    matrix_update[0][2] = new_value
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
